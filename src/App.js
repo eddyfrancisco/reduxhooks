be78ba9655d2840d2bc1d1db760ce620ef3d34c2
Eddy <ed@smartbpo.io>
2019-10-23 01:08:57 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+
+import store from './store';
+
+import CourseList from './components/CourseList';
+
+function App() {
+  return (
+    <Provider store={store}>
+    <div className="App">
+     <CourseList />
+    </div>
+    </Provider>
+  );
+}
+
+export default App;
